af8b37bdb9970ae013ab251ed500a401bca47cd8
Luis Bordin <165961053+LuisBordin@users.noreply.github.com>
2026-06-17 17:26:56 -0300

    Refactor App component to use AuthProvider

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import LoginScreen from "./01-autenticacao/LoginScreen";
-
+import { AuthProvider } from "./core/AuthContext";
+import RootNavigator from "./core/RootNavigator";
 export default function App() {
   return (
     <SafeAreaProvider>
-      <StatusBar style="light" />
-      <LoginScreen />
+      <AuthProvider>
+        <StatusBar style="light" />
+        <RootNavigator />
+      </AuthProvider>
     </SafeAreaProvider>
   );
 }
